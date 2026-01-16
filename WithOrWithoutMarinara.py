ab0ee61549462886ed19251d64c2d06d62683634
Holmes <williamholmes678@gmail.com>
2026-01-16 13:34:45 -0500

    Added Color to Terminal

--- WithOrWithoutMarinara.py
+++ WithOrWithoutMarinara.py
@@ -1,25 +1,46 @@
-#Welcome
+# Welcome
 
-#Libraries Imported Here
+# Libraries Imported Here
 import sys
 import time
 
-print("Welcome Branch - Developer: Will H")
-print("\nWelcome to InfoTechCenter V.1.0")
+# ANSI color codes
+GREEN = "\033[32m"
+CYAN = "\033[36m"
+YELLOW = "\033[33m"
+RED = "\033[31m"
+RESET = "\033[0m"
+BOLD = "\033[1m"
+PURPLE = "\033[35m"
 
+print(BOLD + CYAN + "Welcome Branch - Developer: Will H" + RESET)
+print("\n" + PURPLE + BOLD + "Welcome to InfoTechCenter V.1.0" + RESET)
 
 x = 0
 ellipsis = 0
 
 while x != 20:
-    x += 1  #or x = x+1
-    ellipsisMessage = ("InfoTechCenter OS is Booting" + "." * ellipsis)
+    x += 1
+    ellipsisMessage = (
+        YELLOW
+        + "InfoTechCenter OS is Booting"
+        + "." * ellipsis
+        + RESET
+    )
     ellipsis += 1
+    
     sys.stdout.write("\r\033[K" + ellipsisMessage)
     sys.stdout.flush()
-    time.sleep(.5)
-    if ellipsis == 4:
-         ellipsis = 0
-    if x == 20:
-        print("\nOperating System Booted Up - Retina Scanned - Access Granted")
+    time.sleep(0.5)
     
+    if ellipsis == 4:
+        ellipsis = 0
+    
+    if x == 20:
+        print(
+            "\n"
+            + BOLD
+            + GREEN
+            + "Operating System Booted Up - Retina Scanned - Access Granted"
+            + RESET
+        )
